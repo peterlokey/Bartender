08f95654b756816b01485f43e3577995c36291f6
peterlokey <peter.lokey1@gmail.com>
2019-10-16 02:37:56 -0500

    Initial commit. Created project, set up database, and created User class. Implemented Sign-Up functionality.
    TODO: verify username is available

---
+++ src/main/java/org/launchcode/Bartender/models/data/UserDao.java
@@ -0,0 +1,12 @@
+package org.launchcode.Bartender.models.data;
+
+import org.launchcode.Bartender.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface UserDao extends CrudRepository<User, Integer> {
+}
